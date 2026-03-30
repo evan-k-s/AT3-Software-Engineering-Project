5eac8f964b123c0252938c5d3dd609db2314fc31
esemv07 <ruby.semaganda@gmail.com>
2026-03-30 12:54:05 +1100

    Set up database connection with basic user-accounts table

---
+++ backend/database/data.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __table_name__ = 'user_accounts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    _password_hash = db.Column(db.String(100), nullable=False)
