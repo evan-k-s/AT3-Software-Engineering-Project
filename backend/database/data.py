376c780b69d9f1f441e727c808d5d634527e0158
evan-k-s <semaganda_ev@student.kings.edu.au>
2026-04-10 10:27:02 +1000

    Edit User attributes and add user saving function

--- backend/database/data.py
+++ backend/database/data.py
@@ -14,28 +14,27 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     session_token = db.Column(db.String(50), nullable=False)
     csrf_token = db.Column(db.String(50), nullable=False)
     
-    def __init__(self, id, username, email, created_at, password=None, password_hash=None, session_token=None, csrf_token=None, **kwargs):
+    def __init__(self, username, email, created_at, password=None, password_hash=None, session_token=None, csrf_token=None, **kwargs):
         
         super(User, self).__init__(**kwargs)
 
-        self.__id = id
-        self.__username = username
-        self.__email = email
-        self.__created_at = created_at
+        self.username = username
+        self.email = email
+        self.created_at = created_at
 
         if password:  
-            self.__password = generate_password_hash(password)
+            self.password_hash = generate_password_hash(password)
         elif password_hash:
-            self.__password = password_hash
+            self.password_hash = password_hash
         else:
             raise ValueError("Either password or password_hash must be provided")
         
-        self.__session_token = session_token
-        self.__csrf_token = csrf_token
+        self.session_token = session_token
+        self.csrf_token = csrf_token
 
     
     def __repr__(self):
@@ -45,7 +44,7 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password_input):
-        return check_password_hash(self.password, password_input)
+        return check_password_hash(self.password_hash, password_input)
     
     def generate_token(self):
         return secrets.token_urlsafe(32)
@@ -60,13 +59,17 @@ class User(db.Model):
         self.csrf_token = None
 
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
     def to_dict(self):
         return {
             "id": self.id,
             "username": self.username,
             "email": self.email,
             "created_at": self.created_at.isoformat(),
-            "password_hash": self.password,
+            "password_hash": self.password_hash,
             "session_token": self.session_token,
             "csrf_token": self.csrf_token
         }
