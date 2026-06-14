06e508ca3c3810d9897dcb54db4c6b1d33463e10
evan-k-s <semaganda_ev@student.kings.edu.au>
2026-06-14 12:09:47 +1000

    Query OpenLibrary API with user input

---
+++ frontend/book_search.js
@@ -0,0 +1,33 @@
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("title-search");
+const resultsDropdown = document.getElementById("title-results");
+
+let booksResults = [];
+
+searchButton.addEventListener('click', async function(event) {
+    let search = searchInput.value.trim();
+    if (search.length < 3) {
+        alert("Please enter at least 3 characters to search");
+        return;
+    }
+
+    try {
+        const apiUrl = `https://openlibrary.org/search.json?q=${encodeURIComponent(search)}`;
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        booksResults = data.docs;
+        resultsDropdown.innerHTML = "";
+        booksResults.forEach(book => {
+            const option = document.createElement('option');
+            option.value = book.key;
+            option.textContent = book.title;
+            console.log(option.textContent);
+            resultsDropdown.appendChild(option);
+        });
+    } catch (error) {
+        console.log("Failed to load results");
+    }
+})
