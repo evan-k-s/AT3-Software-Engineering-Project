6c1ca0017473973cc6f42cd458b01e09b7428762
evan-k-s <semaganda_ev@student.kings.edu.au>
2026-06-15 19:14:08 +1000

    Add autofill of specific book data categories (title, author, id)

--- frontend/book_search.js
+++ frontend/book_search.js
@@ -2,6 +2,10 @@ const searchButton = document.getElementById("searchButton");
 const searchInput = document.getElementById("title-search");
 const resultsDropdown = document.getElementById("title-results");
 
+const titleField = document.getElementById("title");
+const authorField = document.getElementById("author");
+const olidField = document.getElementById("olid");
+
 let booksResults = [];
 
 searchButton.addEventListener('click', async function(event) {
@@ -22,12 +26,34 @@ searchButton.addEventListener('click', async function(event) {
         resultsDropdown.innerHTML = "";
         booksResults.forEach(book => {
             const option = document.createElement('option');
-            option.value = `${book.title} by ${book.author_name}`;
-            console.log(option.value);
+            option.value = `${book.title} by ${book.author_name} (${book.cover_edition_key})`;
+            option.setAttribute('data-title', book.title);
+            option.setAttribute('data-author', book.author_name);
+            option.setAttribute('data-olid', book.cover_edition_key);
             resultsDropdown.appendChild(option);
         });
         searchInput.showPicker();
     } catch (error) {
         console.log("Failed to load results");
+    }
+});
+
+
+searchInput.addEventListener('input', function(event) {
+    const selectedBook = event.target.value;
+    const selectedOption = resultsDropdown.querySelector(`option[value="${selectedBook}"]`);
+
+    if (selectedOption) {
+        const title = selectedOption.getAttribute('data-title');
+        const author = selectedOption.getAttribute('data-author');
+        const olid = selectedOption.getAttribute('data-olid');
+
+        titleField.value = title;
+        authorField.value = author;
+        olidField.value = olid;
+    } else {
+        titleField.value = "";
+        authorField.value = "";
+        olidField.value = "";
     }
 })
