4349b490f3bde209ed01cfcada6f60a8c5267f48
evan-k-s <semaganda_ev@student.kings.edu.au>
2026-06-19 11:50:12 +1000

    Check whether book has already been reviewed by user

--- backend/services/review.py
+++ backend/services/review.py
@@ -1,10 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from classes.Error import InputError, AccessError
-from database.data import db, Review
+from database.data import db, Review, User
 
 def user_create_review(user, title, author, olid, rating, review_body):
     if type(rating) != int:
         InputError("Invalid rating!")
+    
+    exists = User.query.join(User.reviews).filter(User.id==user.id, Review.olid==olid).first() is not None
+
+    if exists:
+        raise InputError("Book already reviewed")
 
     new_review = Review(user, title, author, olid, rating, review_body)
 
