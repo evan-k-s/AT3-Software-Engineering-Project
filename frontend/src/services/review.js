5084777861eae4c07d8871c782f3fb786c935464
evan-k-s <semaganda_ev@student.kings.edu.au>
2026-06-20 13:12:20 +1000

    Add confirmation message before review deletion

--- frontend/src/services/review.js
+++ frontend/src/services/review.js
@@ -2,19 +2,23 @@ import { requestBackend } from "./api.js";
 
 
 window.deleteReview = async (review_id) => {
-    console.log("working");
-    try {
-        const tokens = {
-            sessionToken: sessionStorage.getItem("session_token"),
-            csrfToken: sessionStorage.getItem("csrf_token"),
-        };
-        const body = { review_id };
+    const confirmation = confirm("Are you sure you want to delete this review?");
+    if (confirmation) {
+        try {
+            const tokens = {
+                sessionToken: sessionStorage.getItem("session_token"),
+                csrfToken: sessionStorage.getItem("csrf_token"),
+            };
+            const body = { review_id };
 
-        const response = await requestBackend("POST", "delete-review", tokens, body, null);
+            const response = await requestBackend("POST", "delete-review", tokens, body, null);
 
-        window.location.reload();
-    } catch (error) {
-        alert(error);
-        console.error(error);
+            window.location.reload();
+        } catch (error) {
+            alert(error);
+            console.error(error);
+        }
+    } else {
+        return
     }
 };
